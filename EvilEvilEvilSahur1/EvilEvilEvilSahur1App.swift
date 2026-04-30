96971220aec2a7292a19fe83ee7ce0cf9a466c38
BT_C09_10 <mobileappdev@bths.edu>
2026-04-30 14:26:07 -0400

    Initial Commit

---
+++ EvilEvilEvilSahur1/EvilEvilEvilSahur1App.swift
@@ -0,0 +1,17 @@
+//
+//  EvilEvilEvilSahur1App.swift
+//  EvilEvilEvilSahur1
+//
+//  Created by Student on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct EvilEvilEvilSahur1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
